297287940e8b62ecc69a25ca47f07620a3771bf3
Jordan A <jordandayling@gmail.com>
2026-08-12 16:52:39 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        l = 0
+        best = 0
+        for r in range(len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+
+            best = max(best, prices[r] - prices[l])
+
+        return best
+            
+
